a821bb9420df54f1976293e602bc12c3f585d25b
Vasker <v92prog@gmail.com>
2022-08-10 18:12:04 +0300

    correct image name refactoring

--- market_bot/img_refactor.py
+++ market_bot/img_refactor.py
@@ -1,25 +1,52 @@
 import os
 
-"""Входящийе файлы (photo_1@25-07-2022_13-25-11) и (photo_1@25-07-2022_13-25-11_thumb)"""
+"""Входящийе файлы (photo_1@25-07-2022_13-25-11) и (photo_1@25-07-2022_13-25-11_thumb)
+    Файлы с _thumb для телеграмм, без для 1с"""
 
-dir_name = 'products'
-for root, dirs, files in os.walk(dir_name):
+""""Использовать код по очереди"""
+
+root_dir = 'products'
+c_dir = 'products/1c'
+for root, dirs, files in os.walk(root_dir):
     for file in files:
         if file == 'no-image.jpg':
             continue
+        file_oldname = os.path.join(root, file)
+        if '_thumb' not in file:
+            file_destination = f'{root}/1c/{file}'
+            os.replace(f'{root}/{file}', file_destination)
+            continue
 
         if '@' in file and 'rev' not in file:
-            file_oldname = os.path.join(root, file)
-
-            if '_thumb' in file:
+            file_split = file.split('@')
+            name, num = file_split[0].split('_')
+            if int(num) % 2 == 0:
                 """Тыльные строны"""
-                file_split = file.split('@')
-                file_newname_newfile = os.path.join(root, f'{file_split[0]}@rev.jpg')
+                file_newname_newfile = os.path.join(root, f'{name}{int(num) - 1}@rev.jpg')
                 os.rename(file_oldname, file_newname_newfile)
             else:
                 """Лицевые стороны"""
-                file_split = file.split('@')
-                file_newname_newfile = os.path.join(root, f'{file_split[0]}.jpg')
+
+                file_newname_newfile = os.path.join(root, f'{name}{int(num)}.jpg')
                 os.rename(file_oldname, file_newname_newfile)
         else:
             continue
+#
+# for root, dirs, files in os.walk(c_dir):
+#     for file in files:
+#         for file in files:
+#             file_oldname = os.path.join(root, file)
+#             if '@' in file and 'rev' not in file:
+#                 file_split = file.split('@')
+#                 name, num = file_split[0].split('_')
+#                 if int(num) % 2 == 0:
+#                     """Тыльные строны"""
+#                     file_newname_newfile = os.path.join(root, f'{name}{int(num) - 1}@rev.jpg')
+#                     os.rename(file_oldname, file_newname_newfile)
+#                 else:
+#                     """Лицевые стороны"""
+#
+#                     file_newname_newfile = os.path.join(root, f'{name}{int(num)}.jpg')
+#                     os.rename(file_oldname, file_newname_newfile)
+#             else:
+#                 continue
